2370ad1a21609d5b96fcbabe49e6ed8f2a4a91ed
AOtenkaya <otenkayadev@gmail.com>
2024-11-22 13:38:11 +0300

    feat: add neccessary packages and make configs for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Add paths to your files
+  ],
   theme: {
     extend: {},
   },
