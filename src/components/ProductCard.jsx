5035826f44d06f3bb82dd33a4abcf7572a7470c8
AOtenkaya <otenkayadev@gmail.com>
2024-11-24 21:33:25 +0300

    fix: made the fix for mobile devices add cart button visibility and code cleaning and improvement

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -8,15 +8,24 @@ const ProductCard = ({ product }) => {
   const dispatch = useDispatch();
   const { theme } = useContext(ThemeContext); // Access the current theme
 
-  const handleAddToCart = (event) => {
+  const handleAddToCart = () => {
     dispatch(addToCart(product));
+  };
+
+  const themeStyles = {
+    cardBackground:
+      theme === "dark" ? "bg-gray-800 text-white" : "bg-white text-gray-900",
+    priceColor: theme === "dark" ? "text-green-400" : "text-green-600",
+    buttonBackground:
+      theme === "dark"
+        ? "bg-orange-500 hover:bg-orange-600"
+        : "bg-orange-500 hover:bg-orange-600",
+    titleColor: theme === "dark" ? "text-white" : "text-gray-900",
   };
 
   return (
     <div
-      className={`group ${
-        theme === "dark" ? "bg-gray-800 text-white" : "bg-white text-gray-900"
-      } rounded-lg shadow-lg p-4 hover:shadow-2xl transition-all`}
+      className={`group ${themeStyles.cardBackground} rounded-lg shadow-lg p-4 hover:shadow-2xl transition-all`}
     >
       <Link to={`/products/${product.id}`}>
         <img
@@ -27,18 +36,12 @@ const ProductCard = ({ product }) => {
 
         <div className="mt-4">
           <h3
-            className={`${
-              theme === "dark" ? "text-white" : "text-gray-900"
-            } text-lg font-semibold truncate`}
+            className={`${themeStyles.titleColor} text-lg font-semibold truncate`}
           >
             {product.title}
           </h3>
 
-          <p
-            className={`${
-              theme === "dark" ? "text-gray-300" : "text-gray-600"
-            } mt-2`}
-          >
+          <p className={`${themeStyles.priceColor} mt-2 text-xl font-bold`}>
             ${product.price}
           </p>
         </div>
@@ -47,11 +50,7 @@ const ProductCard = ({ product }) => {
       {/* Button */}
       <button
         onClick={handleAddToCart}
-        className={`${
-          theme === "dark"
-            ? "bg-orange-500 hover:bg-orange-600"
-            : "bg-orange-500 hover:bg-orange-600"
-        } group-hover:visible mt-4 w-full text-white p-2 rounded-lg transition-colors invisible`}
+        className={`group-hover:visible mt-4 w-full text-white p-2 rounded-lg transition-colors visible lg:invisible  ${themeStyles.buttonBackground}`}
       >
         Add to Cart
       </button>
