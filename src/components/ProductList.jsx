a88942cf3c3e379516e866bc96134d8f8ce7999e
AOtenkaya <otenkayadev@gmail.com>
2024-11-25 01:40:18 +0300

    style: made 5 product on 2xl screens

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -60,7 +60,7 @@ const ProductList = () => {
         <p className={`mb-6 ${themeStyles.filterMessage}`}>{filterMessage}</p>
       )}
       {/* Product Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-5 gap-3">
         {filteredItems.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
