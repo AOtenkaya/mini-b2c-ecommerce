03babcd6fe3677c57a387abc76548c22672c9f18
AOtenkaya <otenkayadev@gmail.com>
2024-11-24 19:41:11 +0300

    fix: style fix for product name and price and responsiveness

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -41,26 +41,33 @@ const CartItem = React.memo(({ item }) => {
 
   return (
     <div
-      className={`flex items-center justify-between ${themeStyles.backgroundColor} p-4 rounded-lg shadow-md mb-4`}
+      className={`flex flex-col md:flex-row items-start md:items-center justify-between ${themeStyles.backgroundColor} p-4 rounded-lg shadow-md mb-4`}
     >
-      <div className="flex items-center space-x-4 w-fit">
+      {/* Left Section: Product Image and Title */}
+      <div className="flex items-start md:items-center space-x-4 w-full">
+        {/* Product Image */}
         <img
           src={item.image}
           alt={item.title}
           className="w-16 h-16 object-fill rounded-md"
         />
+
+        {/* Product Title and Price */}
         <div className="flex-1">
           <Link to={`/products/${item.id}`} className="block">
-            <h2 className="text-lg font-semibold truncate w-full">
+            <h2 className="text-base md:text-lg font-semibold w-full break-words whitespace-normal">
               {item.title}
             </h2>
           </Link>
-
-          <p className="text-gray-600">${item.price}</p>
+          {/* Adding space between title and price */}
+          <p className="mt-2 text-sm md:text-base font-semibold text-green-600">
+            ${item.price}
+          </p>
         </div>
       </div>
 
-      <div className="flex items-center space-x-4">
+      {/* Right Section: Quantity Controls and Remove Button */}
+      <div className="flex items-center space-x-4 mt-4 md:mt-0">
         <button
           onClick={handleDecrease}
           className={`rounded-full px-2 py-1 ${themeStyles.buttonColor}`}
@@ -68,7 +75,9 @@ const CartItem = React.memo(({ item }) => {
           -
         </button>
 
-        <span className="text-lg font-semibold">{item.quantity}</span>
+        <span className="text-sm md:text-lg font-semibold">
+          {item.quantity}
+        </span>
 
         <button
           onClick={handleIncrease}
@@ -77,7 +86,10 @@ const CartItem = React.memo(({ item }) => {
           +
         </button>
 
-        <button onClick={handleRemove} className="relative group text-xl">
+        <button
+          onClick={handleRemove}
+          className="relative group text-lg md:text-xl"
+        >
           <FaTrash className={themeStyles.buttonColor} />
           <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-1 bg-black text-white text-xs rounded px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity">
             Delete from cart
